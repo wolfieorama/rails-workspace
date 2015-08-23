b392df1a0389d72ca1c6b9b23bca9466fb5213c9
wolfieorama <wachirawamunyi@gmail.com>
2015-08-24 01:34:12 +0300

    added the show view & show method in controller

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -9,6 +9,10 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
+  def show
+    @article= Article.find(params[:id])
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
