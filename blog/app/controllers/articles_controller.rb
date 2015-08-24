351dabce7a04e17f2feb9d5c6c441d259a658750
wolfieorama <wachirawamunyi@gmail.com>
2015-08-24 19:12:50 +0300

    reusing form views and updating / editing

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -4,8 +4,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article= Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -15,15 +23,22 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
     render 'new'
-  end
+    end
   end
 
-  def show
-    @article= Article.find(params[:id])
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
+
 
   private
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
