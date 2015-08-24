7922b736361678d543c35b445a4ddc091b649b1c
wolfieorama <wachirawamunyi@gmail.com>
2015-08-24 22:42:57 +0300

    added hhtp auth for create and delete

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "user", password: "user", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
