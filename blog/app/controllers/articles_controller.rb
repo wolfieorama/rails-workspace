6aab18bc72015ee5f138a7455f5406b80bee9130
wolfieorama <wachirawamunyi@gmail.com>
2015-08-24 12:13:10 +0300

    added error message for  feild validation and CSS highlighting

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,15 +3,19 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+    render 'new'
+  end
   end
 
   def show
