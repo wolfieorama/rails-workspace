4405660c727cec1f8402dcdcef866071e9de8e1c
wolfieorama <wachirawamunyi@gmail.com>
2015-08-26 12:15:41 +0300

    added category model && migrate db

--- jobsboard/db/schema.rb
+++ jobsboard/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825074426) do
+ActiveRecord::Schema.define(version: 20150826091225) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
