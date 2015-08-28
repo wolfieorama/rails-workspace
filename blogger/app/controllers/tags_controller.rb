f5ba370ce0028beb5596f24201c6fc1d2d087c78
wolfieorama <wachirawamunyi@gmail.com>
2015-08-28 11:32:27 +0300

    generated gem file && added destrot on tags

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -1,10 +1,18 @@
 class TagsController < ApplicationController
 
   def index
-    @tags = Tag.all 
+    @tags = Tag.all
   end
 
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    redirect _to tags_path
+
   end
 end
