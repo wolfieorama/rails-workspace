426f11712f7a1f3390b79b7c26716453cdc76bfb
wolfieorama <wachirawamunyi@gmail.com>
2015-09-21 00:56:29 +0300

    validation of stock field and implementing stock helper

--- merchant/app/models/product.rb
+++ merchant/app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
-  validates_numericality_of :price 
+  validates_numericality_of :price
+  validates :stock, numericality: {only_integer:true, greater_than_or_equal_to:0} 
 end
