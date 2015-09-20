59aa0ec784ab0fd204545024d25f13b9e28d2bfe
wolfieorama <wachirawamunyi@gmail.com>
2015-09-21 01:47:08 +0300

    added model relationships to the models

--- merchant/app/models/order_item.rb
+++ merchant/app/models/order_item.rb
@@ -1,2 +1,6 @@
 class OrderItem < ActiveRecord::Base
+  belongs_to :order
+  belongs_to :product
+  validates :order_id, presence: { strict: true }
+  validates :product_id, presence: { strict: true }
 end
--- merchant/app/models/product.rb
+++ merchant/app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  has_many :order_items
   validates_numericality_of :price
-  validates :stock, numericality: {only_integer:true, greater_than_or_equal_to:0} 
+  validates :stock, numericality: {only_integer:true, greater_than_or_equal_to:0}
 end
