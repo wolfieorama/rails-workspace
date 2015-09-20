7152e03d57a31b89cd6de7f25dec03eed866868a
wolfieorama <wachirawamunyi@gmail.com>
2015-09-21 00:27:42 +0300

    added helper for checking stock availability

--- merchant/app/helpers/products_helper.rb
+++ merchant/app/helpers/products_helper.rb
@@ -4,4 +4,12 @@ module ProductsHelper
    number_to_currency price
   end
 
+  def print_stock(stock)
+    if stock > 0
+      content_tag(:span, "In stock", class: "in_stock")
+    else
+      content_tag(:span, "Out of Stock", class: "out_stock")
+    end
+  end
+
 end
