60af8b6737d9981837724d6c11a9acf0abae6e5d
wolfieorama <wachirawamunyi@gmail.com>
2015-09-22 23:42:50 +0300

    edited validation rule to fit better

--- merchant/app/models/order_item.rb
+++ merchant/app/models/order_item.rb
@@ -1,6 +1,6 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
-  validates :order_id, presence: { strict: true }
-  validates :product_id, presence: { strict: true }
+  validates :order_id, presence: true
+  validates :product_id, presence: true
 end
