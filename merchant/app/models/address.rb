3ac8097c9f2b3f2d58fb1cf4cfbf9f9a2f93af03
wolfieorama <wachirawamunyi@gmail.com>
2015-09-27 00:05:51 +0300

    added to_s function for displaying addresses

--- merchant/app/models/address.rb
+++ merchant/app/models/address.rb
@@ -1,7 +1,11 @@
 class Address < ActiveRecord::Base
-  belongs_to :user 
+  belongs_to :user
   validates :line1, presence: true
   validates :city, presence: true
   validates :county, presence: true
   validates :postal_code, presence: true, length: { is: 5 }, numericality: { only_integer: true }
+
+  def to_s
+    [line1, city].compact.join
+  end
 end
