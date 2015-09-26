1ddb47e53757863cb50e7a7c2b28ac0b9b39b4c7
wolfieorama <wachirawamunyi@gmail.com>
2015-09-26 21:57:26 +0300

    added destroy order on session logout

--- merchant/app/controllers/sessions_controller.rb
+++ merchant/app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    session[:order_id] = nil
     redirect_to root_path, notice: "Goodbye!!"
   end
+
 end
