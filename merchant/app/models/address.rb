78e631182134060f55e00e4f6a5208520b7bee35
wolfieorama <wachirawamunyi@gmail.com>
2015-09-26 22:38:38 +0300

    added validation for required fields

--- merchant/app/models/address.rb
+++ merchant/app/models/address.rb
@@ -1,2 +1,6 @@
 class Address < ActiveRecord::Base
+  validates :line1, presence: true
+  validates :city, presence: true
+  validates :county, presence: true
+  validates :postal_code, presence: true
 end
